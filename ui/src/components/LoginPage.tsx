422b7f9979713cd6d219572371910e02ed4176ad
Sabin <sabinonweb7@gmail.com>
2024-02-22 22:37:37 +0545

    Update: Login

--- ui/src/components/LoginPage.tsx
+++ ui/src/components/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Form from "../utils/form";
+import "../index.css";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
@@ -7,39 +8,34 @@ function LoginPage() {
 
   return (
     <>
-      <div className="border-2 flex justify-center flex-col h-80">
-        <div>
-          <h1 className="mb-5 font-extrabold text-3xl">Login</h1>
-        </div>
-        <div className="flex justify-center flex-col items-center">
+      <div className="border-2 bg-sky-400 flex flex-col">
+        <div className="text-3xl font-extrabold p-3">Login</div>
+        <div className="flex flex-col justify-around items-center h-32">
           <Form
-            fieldName="Email: "
-            placeholder="email"
+            placeholder="Username"
+            onChange={(e) => setEmail(e.target.value)}
             value={email}
-            onChange={(
-              e: Event & {
-                target: HTMLFormElement;
-              },
-            ) => setEmail(e.target.value)}
           />
           <Form
-            fieldName="Password: "
-            placeholder="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
             value={password}
-            onChange={(
-              e: Event & {
-                target: HTMLFormElement;
-              },
-            ) => setPassword(e.target.value)}
           />
-          <div className="flex justify-between items-center">
-            <input type="checkbox" />
-            <h3 className="ml-2">Remember Me?</h3>
-          </div>
-          <div>
-            <div className="flex justify-center items-center border-2 w-72 h-10 rounded-full">
-              LogIn
+        </div>
+        <div>
+          <div className="flex justify-center">
+            <div className="flex flex-row justify-between items-center w-72">
+              <div className="flex flex-row justify-center items-center">
+                <input type="checkbox" />
+                <div className="ml-2">Remember me</div>
+              </div>
+              <div>Forgot password</div>
             </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center pb-3">
+          <div className="text-2xl border-2 rounded-full mt-10 w-72 bg-orange-500">
+            LogIn
           </div>
         </div>
       </div>
